0c2b8fff3019b02c82543e0a5adf24736ce9e375
Joshua Gould <gouldj5@students.rowan.edu>
2018-11-18 17:42:12 -0500

    HW4

---
+++ _seniorI/ECOMMS/HW4/Gould_HW4.m
@@ -0,0 +1,26 @@
+%% Homework 4 - Joshua Gould
+% Joshua Gould -
+% ECOMMS - 
+% 11/20/2018
+%% Problem 1 
+% A  Pulse  code  modulation  (PCM)  signal  is  a  binary  signal.  The
+% digital  PCM  is  converted  from  an analog s ignal. The bandwidth of
+% the analog signal is 1kHz. To convert the analog signal to the digital
+% signal, the voltage of each sampling point is divided into 8 levels. (a)
+% What is the baud (symbol rate) for this PCM signal? (b) What is the bit
+% rate for this PCM signal? (c) What  is  the  minimum  bandwidth  for  a
+% communication  system  to  transmit  this  PCM  signal (in unit of kHz)?
+%% Problem 2 - Textbook 3-21
+% A  multilevel  digital  communication  system  sends  one  of  16
+% possible  levels  over  the  channel  every 0.8ms (a) What is the numbe r
+% of bits corresponding to each level ? Or in another word, how many bits
+% in each symbol? Note that the levels in this problem are different from
+% the levels in the last problem.  In  the  last  problem,  levels  are
+% from  analog  to  digital  conversion,  while  in  thi s problem, the
+% digital signal itself is a multilevel signal. (b) What is the baud
+% (symbol rate)? (c) What is he bit rate?
+%% Problem 3 
+% The input of a differential coding signal is 10110010.
+% Begin with the % reference digit “1”, what is the en coded sequence? In your solution,
+% please label, which digit is reference digit, which digits are encoded
+% sequence.
